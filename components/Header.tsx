e69954500c0ade63649f106a46b3a66b5ae70602
Tsotne Pharsenadze <wallemechson@gmail.com>
2024-12-24 14:03:05 +0400

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="bg-white flex justify-between items-center pl-4 pr-4">
+      <h1 className="text-xl font-semibold">memoryApp 🧠</h1>
+      <nav>
+        <ul className="flex items-center justify-center uppercase">
+          <li className="hover:bg-gray-50 p-2">
+            <Link href="/memoryApp/login/">Login</Link>
+          </li>
+          <li className="hover:bg-gray-50 p-2">
+            <Link href="/memoryApp/register/">register</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
