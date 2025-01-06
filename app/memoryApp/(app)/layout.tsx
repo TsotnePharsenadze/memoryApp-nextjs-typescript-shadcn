a0e6c72dc7aec1d9c6220c09a32b0d89571b51aa
Tsotne Pharsenadze <wallemechson@gmail.com>
2025-01-06 16:03:51 +0400

    enabling auth

--- app/memoryApp/(app)/layout.tsx
+++ app/memoryApp/(app)/layout.tsx
@@ -1,23 +1,23 @@
 "use client";
 
-// import Spinner from "@/components/Spinner";
-// import { useSession } from "next-auth/react";
-// import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
+import Spinner from "@/components/Spinner";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  // const router = useRouter();
-  // const session = useSession();
+  const router = useRouter();
+  const session = useSession();
 
-  // useEffect(() => {
-  //   if (session?.status == "unauthenticated") {
-  //     router.push("/memoryApp/login");
-  //   }
-  // }, [session?.status, router]);
+  useEffect(() => {
+    if (session?.status == "unauthenticated") {
+      router.push("/memoryApp/login");
+    }
+  }, [session?.status, router]);
 
-  // if (session?.status == "loading") return <Spinner />;
+  if (session?.status == "loading") return <Spinner />;
 
-  // if (session?.status == "unauthenticated") return null;
+  if (session?.status == "unauthenticated") return null;
 
   return children;
 };
--- components/game/GameCard.tsx
+++ components/game/GameCard.tsx
@@ -10,6 +10,7 @@ import {
   CardHeader,
   CardTitle,
 } from "../ui/card";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 const GameCard = ({
@@ -29,10 +30,15 @@ const GameCard = ({
   bgColor: string;
   imageAlt: string;
 }) => {
+  const session = useSession();
   const router = useRouter();
 
   const goTo = (href: string) => {
-    router.push(`/memoryApp/${href.toLowerCase()}`);
+    if (session?.status == "authenticated") {
+      router.push(`/memoryApp/${href.toLowerCase()}`);
+    } else if (session?.status == "unauthenticated") {
+      router.push(`/memoryApp/login`);
+    }
   };
 
   return (
